7645aac4e43ff3a96ffe0ac85b79f964483fe2c9
Hugo Giraudel <hugo.giraudel@gmail.com>
2015-06-29 10:03:44 +0200

    Created the main source file

---
+++ src/image-preloader.js
@@ -0,0 +1,107 @@
+(function (global) {
+  function defer() {
+    var resolve, reject, promise = new Promise(function (a, b) {
+      resolve = a;
+      reject = b;
+    });
+
+    return {
+      resolve: resolve,
+      reject: reject,
+      promise: promise
+    };
+  }
+
+  /**
+   * Image preloader
+   * @param {Object} options
+   */
+  var ImagePreloader = function (options) {
+    this.options = options || {};
+    this.options.parallel = options.parallel || false;
+    this.items = [];
+  };
+
+  /**
+   * @param  {Array} array - Array of images to add the queue
+   * @return {Promise}
+   */
+  ImagePreloader.prototype.queue = function (array) {
+    var d = defer();
+
+    this.items.push({
+      collection: array,
+      deferred: d
+    });
+    
+    return d.promise;
+  };
+
+  /**
+   * Preload a single image
+   * @param  {String} path - Image url
+   * @return {Promise}
+   */
+  ImagePreloader.prototype.preloadImage = function (path) {
+    return new Promise(function (resolve, reject) {
+      var image = new Image();
+      image.onload = resolve;
+      image.onerror = resolve;
+      image.src = path;
+    });
+  };
+
+  /**
+   * Preload the whole queue (sequencially or in parallel depending on the value
+   * of `this.options.parallel`)
+   */
+  ImagePreloader.prototype.preload = function () {
+    if (this.options.parallel) {
+      this.preloadParallel();
+    } else {
+      this.preloadSequence();
+    }
+
+    this.done();
+  };
+
+  /**
+   * Resolve the promise for each image collection in the queue
+   */
+  ImagePreloader.prototype.done = function () {
+    this.items.forEach(function (item) {
+      Promise.all(item.collection)
+        .then(function () { item.deferred.resolve() })
+        .catch(console.log.bind(console));
+      });
+  };
+
+  /**
+   * Preload the queue in parallel
+   */
+  ImagePreloader.prototype.preloadParallel = function () {}
+    var max = Math.max.apply(Math, this.items.map(function (el) { return el.collection.length; }));
+    
+    for (var i = 0; i < max; i++) {
+      this.items.forEach(function (item) {
+        if (typeof item.collection[i] !== 'undefined') {
+          item.collection[i] = this.preloadImage(item.collection[i])
+        }
+      }, this);
+    }
+  };
+
+  /**
+   * Preload the queue in sequence
+   */
+  ImagePreloader.prototype.preloadSequence = function () {}
+    this.items.forEach(function (item) {
+      item.collection.forEach(function (image, i) {
+        item.collection[i] = this.preloadImage(image);
+      }, this);
+    }, this);
+  };
+
+  global.ImagePreloader = ImagePreloader;
+
+}(window));
